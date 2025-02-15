6404acb01852d94b2a7f0a0490395cabbd9d1399
gofla1996 <110741@daum.net>
2025-02-16 02:16:18 +0900

    [구성] 환경구성1

---
+++ vite.config.ts
@@ -0,0 +1,44 @@
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+import { reactClickToComponent } from 'vite-plugin-react-click-to-component';
+
+const viteConfig = defineConfig((env) => {
+  const isDevMode = env.mode.includes('development');
+
+  return {
+    base: '/likelion-react-homework6/',
+    plugins: [
+      react({
+        jsxRuntime: 'automatic',
+      }),
+      reactClickToComponent(),
+      tailwindcss(),
+    ],
+    server: {
+      host: 'localhost',
+      port: 3000,
+    },
+    preview: {
+      host: 'localhost',
+      port: 8080,
+    },
+    css: {
+      devSourcemap: true,
+      modules: {
+        localsConvention: 'camelCase',
+        generateScopedName: isDevMode
+          ? '_[local]_[hash:base64:3]'
+          : '_[hash:base64:6]',
+      },
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  };
+});
+
+export default viteConfig;
